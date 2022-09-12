b18d2e9496bebcfd7961e83d462e25eb9bfc10e6
rohit9172 <104486787+rohit9172@users.noreply.github.com>
2022-09-12 20:00:20 +0530

    Update test_sum.py

--- test_sum.py
+++ test_sum.py
@@ -1,12 +1,24 @@
 import unittest
 
-from sum import sumofnum
+from sum import multiply, subofnum, sumofnum
 
 class sumNum(unittest.TestCase):
     def test_sum(self):
         a= 3
         b=4
         response = sumofnum(a,b)
+        self.assertEqual(response, a+b)
+    
+    def test_sub(self):
+        a= 3
+        b=4
+        response = subofnum(a,b)
+        self.assertEqual(response, a-b)
+    
+    def test_sum(self):
+        a= 3
+        b=4
+        response = multiply(a,b)
         self.assertEqual(response, a+b)
 
 if __name__ == '__main__':
