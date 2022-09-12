22930d424b60f2de3029adb69b106a1907452e18
rohit9172 <104486787+rohit9172@users.noreply.github.com>
2022-09-12 15:59:27 +0530

    basic

---
+++ test_sum.py
@@ -0,0 +1,13 @@
+import unittest
+
+from sum import sumofnum
+
+class sumNum(unittest.TestCase):
+    def test_sum(self):
+        a= 3
+        b=4
+        response = sumofnum(a, b)
+        self.assertEqual(response, a+b)
+
+if __name__ == '__main__':
+    unittest.main()
